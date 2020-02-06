7dc9384ee71e1523eb226f62ce8e3309a0039a12
victor <victorexposito@hotmail.dk>
2020-02-06 10:13:25 +0100

    test

---
+++ Class1/1._Intro/Intro_Exercises/2.Strings.js
@@ -0,0 +1,34 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+var added = parseFloat(numberOne) + parseFloat(numberTwo)
+console.log(added);
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+var one = 10;
+var two = 45;
+var three = 98;
+
+var sum = parseFloat(one+two+three);
+var average = 153/3;
+console.log(parseFloat(average).toFixed(3))
+
+// Show in the console the avg. with 5 decimals
+
+// --------------------------------------
