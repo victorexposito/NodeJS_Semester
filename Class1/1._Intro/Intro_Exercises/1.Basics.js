35a4c6e69955cb3f6b3db2fadc86e96aa5576af0
victor <victorexposito@hotmail.dk>
2020-02-06 09:50:27 +0100

    First commit

---
+++ Class1/1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,18 @@
+var firstName = "Anders";
+var lastName = "Latif";
+
+
+console.log("My first name is", firstName, "and my last name is", lastName)
+// show in the console
+// My first name is Anders and my last name is Latif
+
+
+
+var year = "2017";
+var number = 1;
+
+var added = parseInt(year)+number
+
+console.log(added)
+
+//Add the year plus the number
